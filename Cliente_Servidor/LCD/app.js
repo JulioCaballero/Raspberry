47184ed0506c99444cbeb606e3aa05c1956c4058
Julio Caballero <173180@ids.upchiapas.edu.mx>
2019-03-01 21:14:42 -0600

    ...

--- Cliente_Servidor/LCD/app.js
+++ Cliente_Servidor/LCD/app.js
@@ -4,7 +4,11 @@ var app = require('express')(),
 	http = require('http').createServer(app),
 	io = require('socket.io')(http),
 	port = process.env.PORT || 3000,
-	
+	five = require("johnny-five"),
+    Raspi = require("raspi-io"),
+    board = new five.Board({
+        io: new Raspi()
+    })
 	
 
     HOST = '127.0.0.1',
@@ -22,6 +26,17 @@ app
 
 io.on('connection', (socket) => {
 	socket.on('mensaje', (data) => {
-		console.log(data)
+        //console.log(data)
+        var lcd = new five.LCD({
+            controller: "JHD1313M1"
+          });
+        
+          lcd.useChar("heart");
+        
+          lcd.cursor(0, 0).print("hello :heart:");
+        
+          lcd.blink();
+        
+          lcd.cursor(1, 0).print("Blinking? ");
 	})	
 })
