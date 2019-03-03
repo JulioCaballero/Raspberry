453e404b22c54f4e6675ac0ca173fdbf16883a81
Julio Caballero <173180@ids.upchiapas.edu.mx>
2019-03-03 11:11:12 -0600

    python lcd

--- Cliente_Servidor/LCD/main.py
+++ Cliente_Servidor/LCD/main.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
-
+import I2C_LCD_driver
+from time import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+mylcd = I2C_LCD_driver.lcd()
 
 @app.route('/')
 def index():
@@ -13,6 +15,7 @@ def index():
 @socketio.on('mensaje')
 def handleMessage(msg):
     print('mensaje: ' + msg)
-    
+    mylcd.lcd_display_string(msg, 1)
+
 if __name__ == '__main__':
     socketio.run(app,debug=True)
